ca335118580fa2629d8b2ba48f4fe9a0f0000c5b
Gram <master_fess@mail.ru>
2020-03-03 16:08:46 +0100

    init hello

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+)
+
+type Config struct {
+	Pretty     bool
+	GraphiQL   bool
+	Playground bool
+	Host       string
+	Port       int
+}
+
+func main() {
+	// Schema
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+		// Playground: true,
+	})
+
+	http.Handle("/graphql", h)
+	fmt.Println("listening")
+	err = http.ListenAndServe(":8080", nil)
+	log.Fatalf("server has stopped: %v", err)
+}
